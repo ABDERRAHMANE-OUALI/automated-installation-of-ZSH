81be09eda861395535386ea7e88827905afe8bd7
Abde Arrahmane Ouali <60064144+ANDRUXUIS@users.noreply.github.com>
2020-10-11 19:45:30 +0100

    Just a Automation File to Push Code to github written in Bash,

--- github.sh
+++ github.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git add . && git add .
-git commit -m "Added Some Improvement to README File"
+git commit -m "Added Some Improvement to README File and Labeled it to ignore it"
 git push -f
